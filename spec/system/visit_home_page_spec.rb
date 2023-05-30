31c01451d79db90c16247c00d0799dc5b9f946d1
João Almeida <joao@campuscode.com.br>
2023-05-30 17:47:38 -0300

    WIP Usuário faz login

--- spec/system/visit_home_page_spec.rb
+++ spec/system/visit_home_page_spec.rb
@@ -6,4 +6,17 @@ describe 'Usuário visita home' do
 
     expect(page).to have_content('SuperHero App')
   end
+
+  it 'usuário faz login' do
+    User.create!(email: 'hero@email.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'email', with: 'hero@email.com'
+    fill_in 'password', with: '123456'
+    click_on 'Login'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content('Login feito com sucesso')
+  end
 end
