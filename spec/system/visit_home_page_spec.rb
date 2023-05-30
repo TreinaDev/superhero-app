e889007834f8fd9d4d35588bbc0df513cb482b5f
João Almeida <joao@campuscode.com.br>
2023-05-30 17:56:39 -0300

    Mudando os textos do fluxo de login

--- spec/system/visit_home_page_spec.rb
+++ spec/system/visit_home_page_spec.rb
@@ -11,10 +11,10 @@ describe 'Usuário visita home' do
     User.create!(email: 'hero@email.com', password: '123456')
 
     visit root_path
-    click_on 'Entrar'
+    click_on 'Login'
     fill_in 'email', with: 'hero@email.com'
     fill_in 'password', with: '123456'
-    click_on 'Login'
+    click_on 'Entrar'
 
     expect(current_path).to eq root_path
     expect(page).to have_content('Login feito com sucesso')
